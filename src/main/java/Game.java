9d0899da4b232764c5cce269d586297d32de0d16
yeonwoo.kang <kywoo26@naver.com>
2024-04-29 10:56:49 +0900

    cycle 3

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -7,5 +7,11 @@ public class Game {
         if (guessNumber.length() != 3) {
             throw new IllegalArgumentException();
         }
+
+        for (char number : guessNumber.toCharArray()) {
+            if (!Character.isDigit(number)) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 }
